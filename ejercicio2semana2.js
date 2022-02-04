556d8662a9a97b5369edc5ebd766b7c3cf514b8a
Kevin Roldan <22006706@galileo.edu>
2022-02-04 17:19:27 -0600

    firts comment

---
+++ ejercicio2semana2.js
@@ -0,0 +1,35 @@
+var numero = prompt("ingrese un digito del uno al doce")
+
+switch(numero) {
+	case 1:
+	case 2:
+	case 12:
+		console.log('es invierno');
+	
+		break;
+
+	case 3:
+	case 4:
+	case 5:
+		console.log('es primavera');		
+	
+		break;
+
+	case 6:
+	case 7:
+	case 8:
+		console.log('es verano');
+
+		break;
+
+	case 9:
+	case 10:
+	case 11:
+		console.log('es otoño');
+
+		break;
+	default:
+		console.log('este numero no es admitido');
+
+console.log('22006706 Kevin Roldan Santos');
+}
